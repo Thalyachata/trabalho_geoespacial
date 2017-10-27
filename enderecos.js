f44181382cf2228990c6f5cddd869b97d49e7b8f
Thalia <thaliasantos2931@gmail.com>
2017-10-27 09:40:47 -0200

    trabalho

---
+++ enderecos.js
@@ -0,0 +1,157 @@
+db.Lugares.insert( {
+    nome: "Mércia",
+   localização: { digite: "Ponto", coordenadas: [ -43.0685685, -15.5343928 ] },
+
+} );
+db.Lugares.insert({
+	nome:"Luana",
+	localização:{digite: "Ponto", coordenadas:[-43.3160809, -15.8052074]}
+});
+db.Lugares.insert({
+	nome:"Joana",
+	localização:{digite:"Ponto", coordenadas:[-43.310403, -15.7943479]}
+});
+db.Lugares.insert({
+	nome:"Alberto",
+	localização:{digite:"Ponto", coordenadas:[-43.3179225, -15.8057438]}
+
+});
+db.Lugares.insert({
+	nome:"Layla",
+	localização:{digite:"Ponto", coordenadas:[-43.3179687, -15.8198619]}
+
+});
+db.Lugares.insert({
+	nome:"Thalia",
+	localização:{digite:"Ponto", coordenadas:[-43.341919, -15.792172]}
+
+});
+db.Lugares.insert({
+	nome:"Emilly",
+	localização:{digite:"Ponto", coordenadas:[-43.312611, -15.807591]}
+
+});
+db.Lugares.insert({
+	nome:"Sávia",
+	localização:{digite:"Ponto", coordenadas:[-43.3223997, -15.781380]}
+
+});
+db.Lugares.insert({
+	nome:"Dieyse",
+	localização:{digite:"Ponto", coordenadas:[-43.323578, -15.781380]}
+
+});
+db.Lugares.insert({
+	nome:"Vitória Ingred",
+	localização:{digite:"Ponto", coordenadas:[-43.314893, -15.8147075]}
+
+});
+db.Lugares.insert({
+	nome:"João Vitor",
+	localização:{digite:"Ponto", coordenadas:[-43.306551, -15.8147075]}
+
+});
+db.Lugares.insert({
+	nome:"Michelle",
+	localização:{digite:"Ponto", coordenadas:[-43.3123914, -15.8263155]}
+
+});
+db.Lugares.insert({
+	nome:"Maria Thais",
+	localização:{digite:"Ponto", coordenadas:[-43.0288791, -15.7377377]}
+
+});
+db.Lugares.insert({
+	nome:"Ádrian",
+	localização:{digite:"Ponto", coordenadas:[-43.304557, -15.811190]}
+
+});
+db.Lugares.insert({
+	nome:"Lara",
+	localização:{digite:"Ponto", coordenadas:[-43.3153666, -15.8197566]}
+
+});
+db.Lugares.insert({
+	nome:"Camila",
+	localização:{digite:"Ponto", coordenadas:[-43.2960149, -15.8251198]}
+
+});
+db.Lugares.insert({
+	nome:"Lívia",
+	localização:{digite:"Ponto", coordenadas:[-43.314748, -15.7953561]}
+
+});
+db.Lugares.insert({
+	nome:"Micaelle",
+	localização:{digite:"Ponto", coordenadas:[-43.3148092, -15.8221044]}
+
+});
+db.Lugares.insert({
+	nome:"Flora",
+	localização:{digite:"Ponto", coordenadas:[-43.3051085, -15.8018395]}
+
+});
+db.Lugares.insert({
+	nome:"Jade",
+	localização:{digite:"Ponto", coordenadas:[-43.309728, -15.819087]}
+
+});
+db.Lugares.insert({
+	nome:"Sidney",
+	localização:{digite:"Ponto", coordenadas:[-43.3121088, -15.8036004]}
+
+});
+db.Lugares.insert({
+	nome:"Lavínia",
+	localização:{digite:"Ponto", coordenadas:[-43.3168428, -15.8045297]}
+
+});
+db.Lugares.insert({
+	nome:"Victória Felipe",
+	localização:{digite:"Ponto", coordenadas:[-43.300751, -15.818761]}
+
+});
+
+
+db.Lugares.insert({
+	nome:"Emille Beatriz",
+	localização:{digite: "Ponto", coordenadas:[-43.3125666, -15.8039344]}
+
+});
+db.Lugares.insert({
+	nome:"Hellen Aline",
+	localização:{digite: "Ponto", coordenadas:[-43.3165794, -15.7990549]}
+
+});
+db.Lugares.insert({
+	nome:"Joyce Marianne",
+	localização:{digite: "Ponto", coordenadas:[-43.3087511, -15.8290764]}
+
+});
+db.Lugares.insert({
+	nome:"Amanda Daniel",
+	localização:{digite: "Ponto", coordenadas:[-43.314899, -15.802350]}
+
+});
+db.Lugares.insert({
+	nome:"Luan Alves",
+	localização:{digite: "Ponto", coordenadas:[-43.3138138, -15.8007558]}
+
+});
+db.Lugares.insert({
+	nome:"João Gabriel",
+	localização:{digite: "Ponto", coordenadas:[-43.3049733, -15.8489078]}
+
+});
+db.Lugares.insert({
+	nome:"Daniel Barbosa",
+	localização:{digite: "Ponto", coordenadas:[-43.3084923, -15.8025955]}
+
+});
+db.Lugares.insert({
+	nome:"Tom Roberto",
+	localização:{digite: "Ponto", coordenadas:[-43.3090244, -15.802596]}
+
+});
+
+
